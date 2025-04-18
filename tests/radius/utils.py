4dad064f212143a9a25d78d028774bcad3d0b087
dt-nexthop <dt@nexthop.ai>
2025-04-18 11:59:23 -0400

    Revert "update to use ip get, over ip route"
    This reverts commit 11457d550d3cf8b704c5d08d290e644c5f5a960e.

--- tests/radius/utils.py
+++ tests/radius/utils.py
@@ -126,17 +126,20 @@ def start_tcpdump_and_try_login(
      - Analyze packet capture
     """
 
-    # using ip get here to make sure we are including ip rules that might be in play
     route_json = json.loads(
         duthost.command(
-            "ip -j route get {}".format(ptfhost_mgmt_ip)
+            "show ip route {} json".format(ptfhost_mgmt_ip)
         )["stdout"]
     )
 
     # make sure atleast one route was returned
-    pytest_assert(len(route_json.values), "no route available to PTF")
+    pytest_assert(route_json.values())
 
-    tcpdump_int = route_json[0]["dev"]
+    """
+    the RADIUS server is running on the PTF mgmt interface
+    so the first route here should be the correct one
+    """
+    tcpdump_int = next(iter(route_json.values()))[0]["nexthops"][0]["interfaceName"]
 
     tcpdump_command = (
         "sudo timeout {timeout} tcpdump -i {intf} port 1812 -w {dut_cap_file}"
