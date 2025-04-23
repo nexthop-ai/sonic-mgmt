5e5dbef985f62155f17811ec3d1ac6bbc95b93f7
Venu <venu@nexthop.ai>
2025-04-23 13:49:52 -0700

    Minor re-org

--- tests/fib/test_fib.py
+++ tests/fib/test_fib.py
@@ -877,6 +877,8 @@ def test_ecmp_group_member_flap(
         is_python3=True
     )
 
+    filtered_ports.pop()
+
     # --- Bring the port back up and verify ---
     logging.info("Bringing the uplink port back up.")
 
@@ -884,7 +886,6 @@ def test_ecmp_group_member_flap(
     duthosts[0].shell("sudo config interface {} startup {}".format(asic_ns, nh_dut_ports[0][1]))
 
     time.sleep(60)  # Allow time for the state to stabilize
-    filtered_ports.pop()
     # --- Re-run the PTF test after member is back up ---
     logging.info("Re-verifying ECMP behavior after member up.")
     new_fib_files2 = fib_info_files_per_function(
