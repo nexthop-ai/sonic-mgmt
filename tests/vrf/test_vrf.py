384d6111e1722c9d1d2bc2c1a94707d0f30ac6df
Kalash Nainwal <kalash@nexthop.ai>
2025-04-25 00:30:52 +0000

    fixing lint issue

--- tests/vrf/test_vrf.py
+++ tests/vrf/test_vrf.py
@@ -1047,7 +1047,7 @@ class TestVrfLoopbackIntf():
 
         # Get the current BGP AS number if exists
         bgp_as = duthost.shell("vtysh -c 'show running-config' | grep 'router bgp' | head -1",
-                                module_ignore_errors=True)['stdout']
+                               module_ignore_errors=True)['stdout']
         if not bgp_as or not re.search(r'router bgp (\d+)', bgp_as):
             # If BGP is not configured or we couldn't extract the AS number, create a new BGP router
             logger.warning("No BGP configuration found or couldn't extract AS number, creating default")
