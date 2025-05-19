5b5073fc7e289e0e377431791ed755b63e6a2907
Selva <selva@nexthop.ai>
2025-05-19 10:56:38 -0700

    ISS-1747 [frr unified mode] test_bgp_router_id.py failure (#235)
    Roocause:
        - The deploy-mg not setting Loopback0 as router-id
        - The deploy-mg not adding Loopback0 address to advertise
        - router-id config schema change in UMF
        - router-id config difference between legacy and UMF
    
      Resolution:
        - In legacy mode, without Loopback0 the sessions will not come up.
          When it finds Loopaback0, it will be set as the router-id.
          Updated deploy-mg script to configure Loopback0 address
          as router-id for similar behavior.
        - In legacy mode, the Loopaback0 IP indirectly
          added to routing table using 'network' command.
          But in UMF, this needs to be configured with yang
          schema to BGP_GLOBALS_AF_NETWORK table.
          Updated the deploy-mg script to include the Loopback0
          addresses to BGP_GLOBALS_AF_NETWORK table.
        - In legacy mode, the router-id will be stored in DEVICE_METADATA
          table.  But in UMF, it should be BGP_GLOBALS table.
          Also, in legacy mode we need to restart BGP to take effect the
          router-id changes.  It is not required in UMF.

--- tests/common/helpers/bgp.py
+++ tests/common/helpers/bgp.py
@@ -31,14 +31,19 @@ def run_bgp_facts(duthost, enum_asic_index):
         assert v['state'] == 'established'
         # Verify local ASNs in bgp sessions
         assert v['local AS'] == int(config_facts['DEVICE_METADATA']['localhost']['bgp_asn'].encode().decode("utf-8"))
-        # Check bgpmon functionality by validate STATE DB contains this neighbor as well
-        state_fact = duthost.shell('{} STATE_DB HGET "NEIGH_STATE_TABLE|{}" "state"'
-                                   .format(sonic_db_cmd, k), module_ignore_errors=False)['stdout_lines']
-        peer_type = duthost.shell('{} STATE_DB HGET "NEIGH_STATE_TABLE|{}" "peerType"'
-                                  .format(sonic_db_cmd, k),
-                                  module_ignore_errors=False)['stdout_lines']
-        assert state_fact[0] == "Established"
-        assert peer_type[0] == "i-BGP" if v['remote AS'] == v['local AS'] else "e-BGP"
+        if duthost.get_frr_mgmt_framework_config():
+            # bgpmon is part of bgpcfgd and not applicable for frr mgmt
+            # hence, validate using direct (vtysh) statistics
+            nbinfo = duthost.get_bgp_neighbor_info(k)
+            assert nbinfo['bgpState'] == "Established"
+        else:
+            # Check bgpmon functionality by validate STATE DB contains this neighbor as well
+            state_fact = duthost.shell('{} STATE_DB HGET "NEIGH_STATE_TABLE|{}" "state"'.format(sonic_db_cmd, k),
+                                       module_ignore_errors=False)['stdout_lines']
+            peer_type = duthost.shell('{} STATE_DB HGET "NEIGH_STATE_TABLE|{}" "peerType"'.format(sonic_db_cmd, k),
+                                      module_ignore_errors=False)['stdout_lines']
+            assert state_fact[0] == "Established"
+            assert peer_type[0] == "i-BGP" if v['remote AS'] == v['local AS'] else "e-BGP"
 
     # In multi-asic, would have 'BGP_INTERNAL_NEIGHBORS' and possibly no 'BGP_NEIGHBOR' (ebgp) neighbors.
     nbrs_in_cfg_facts = {}
