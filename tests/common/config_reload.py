913e8550e781a3b360357d2535f0a2d7a4bbb9db
Ronan Mac Fhlannchadha <ronan@nexthop.ai>
2025-12-09 17:57:35 -0800

    resolve tests/common/config_reload.py
    remove the redirects to /dev/null/, appears sudo still needed.

--- tests/common/config_reload.py
+++ tests/common/config_reload.py
@@ -175,11 +175,7 @@ def config_reload(sonic_host, config_source='config_db', wait=120, start_bgp=Tru
             is_buffer_model_dynamic = (output and output.get('stdout') == 'dynamic')
         else:
             is_buffer_model_dynamic = False
-<<<<<<< HEAD
         cmd = 'sudo config load_minigraph -y'
-=======
-        cmd = 'config load_minigraph -y'
->>>>>>> upstream/master
         if traffic_shift_away:
             cmd += ' -t'
         if override_config or macsec_en:
@@ -195,20 +191,12 @@ def config_reload(sonic_host, config_source='config_db', wait=120, start_bgp=Tru
         sonic_host.shell('sudo config save -y')
 
     elif config_source == 'config_db':
-<<<<<<< HEAD
-        cmd = 'sudo config reload -y &>/dev/nul'
-=======
-        cmd = 'config reload -y'
->>>>>>> upstream/master
+        cmd = 'sudo config reload -y'
         reloading = False
         if config_force_option_supported(sonic_host):
             if wait_before_force_reload:
                 reloading = wait_until(wait_before_force_reload, 10, 0, _config_reload_cmd_wrapper, cmd, "/bin/bash")
-<<<<<<< HEAD
-            cmd = 'sudo config reload -y -f  &>/dev/nul'
-=======
-            cmd = 'config reload -y -f'
->>>>>>> upstream/master
+            cmd = 'sudo config reload -y -f'
         if not reloading:
             time.sleep(30)
             sonic_host.shell(cmd, executable="/bin/bash")
@@ -218,15 +206,9 @@ def config_reload(sonic_host, config_source='config_db', wait=120, start_bgp=Tru
         if sonic_host.is_multi_asic:
             for asic in range(sonic_host.num_asics()):
                 golden_path = f'{golden_path},/etc/sonic/running_golden_config{asic}.json'  # noqa: E231
-<<<<<<< HEAD
-        cmd = f'sudo config reload -y -l {golden_path} &>/dev/nul'
+        cmd = f'sudo config reload -y -l {golden_path}'
         if config_force_option_supported(sonic_host):
-            cmd = f'sudo config reload -y -f -l {golden_path} &>/dev/nul'
-=======
-        cmd = f'config reload -y -l {golden_path}'
-        if config_force_option_supported(sonic_host):
-            cmd = f'config reload -y -f -l {golden_path}'
->>>>>>> upstream/master
+            cmd = f'sudo config reload -y -f -l {golden_path}'
         sonic_host.shell(cmd, executable="/bin/bash")
 
     modular_chassis = sonic_host.get_facts().get("modular_chassis")
