1f4d818edddf7d58a3fb23b44dd9cb0eec1e12c2
devesh-nexthop <250206677+devesh-nexthop@users.noreply.github.com>
2026-02-18 22:21:18 -0800

    NOS-4919: add tuple check (#1051)
    <!--
    Please make sure you've read and understood our contributing guidelines;
    https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
    
    Please provide following information to help code review process a bit
    easier:
    -->
    ### Description of PR
    <!--
    - Please include a summary of the change and which issue is fixed.
    - Please also include relevant motivation and context. Where should
    reviewer start? background context?
    - List any dependencies that are required for this change.
    -->
    With scapy update in master and 202511, we see that it returns end
    marker too with options and it is not tuple.
    This causes an exception.
    
    Summary:
    Fixes # (issue)
    
    ### Type of change
    
    <!--
    - Fill x for your type of change.
    - e.g.
    - [x] Bug fix
    -->
    
    - [x] Bug fix
    - [ ] Testbed and Framework(new/improvement)
    - [ ] New Test case
        - [ ] Skipped for non-supported platforms
    - [ ] Test case improvement
    
    
    ### Back port request
    - [ ] 202505
    - [x] 202511
    
    ### Approach
    #### What is the motivation for this PR?
    Test throws exception when run with latest image.
    
    #### How did you do it?
    Check for tuple before extracting option and its value.
    
    #### How did you verify/test it?
    <img width="1492" height="294" alt="image"
    src="https://github.com/user-attachments/assets/29464c74-2d35-4168-aa66-955aeca4dbfd"
    />
    
    #### Any platform specific information?
    
    #### Supported testbed topology if it's a new test case?
    
    ### Documentation
    <!--
    (If it's a new feature, new test case)
    Did you update documentation/Wiki relevant to your implementation?
    Link to the wiki page?
    -->

--- tests/common/dhcp_relay_utils.py
+++ tests/common/dhcp_relay_utils.py
@@ -242,9 +242,11 @@ def calculate_counters_per_pkts(pkts, is_v6=False):
                 elif scapy.DHCP6_RelayReply in pkt:
                     message_type_int = pkt[scapy.DHCP6_RelayReply].msgtype  # Relay-Reply
             else:
-                for opt, val in pkt[scapy.DHCP].options:
-                    if opt == "message-type":
-                        message_type_int = val
+                for data in pkt[scapy.DHCP].options:
+                    if isinstance(data, tuple):
+                        opt, val = data
+                        if opt == "message-type":
+                            message_type_int = val
             message_type_str = (
                 (SUPPORTED_DHCPV6_TYPE if is_v6 else SUPPORTED_DHCPV4_TYPE)[message_type_int - 1]
                 if message_type_int is not None and message_type_int > 0
