e3e8096e73326fd40503f223cef3370663e59141
pinky-nexthop <pinky@nexthop.ai>
2026-02-20 10:59:29 +0530

    NOS-4235: Add 10 sec sleep for counters to be polled (#1066)
    <!--
    Please make sure you've read and understood our contributing guidelines;
    https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
    
    Please provide following information to help code review process a bit
    easier:
    -->
    ### Description of PR
    <!--
    - Please include a summary of the change and which issue is fixed.
    - Please also include relevant motivation and context. Where should
    reviewer start? background context?
    - List any dependencies that are required for this change.
    -->
    
    Summary:
    Fixes # (issue)
    NOS-4235
    
    ### Type of change
    
    <!--
    - Fill x for your type of change.
    - e.g.
    - [x] Bug fix
    -->
    
    - [x] Bug fix
    - [ ] Testbed and Framework(new/improvement)
    - [ ] New Test case
        - [ ] Skipped for non-supported platforms
    - [ ] Test case improvement
    
    
    ### Back port request
    - [ ] 202505
    - [x] 202511
    
    ### Approach
    #### What is the motivation for this PR?
    testQosSaiDscpQueueMapping fails intermittently while getting the
    counters
    
    ```           
    time.sleep(3)
     port_results, queue_results = sai_thrift_read_port_counters(self.dst_client, asic_type, sai_dst_port_id)```
    
    The polling interval for queue stats is 10seconds, so adding a sleep of 10 seconds here. 
    I am unable to repro the issue and this is a hopeful fix. 
    
    #### How did you do it?
    
    #### How did you verify/test it?
    
    #### Any platform specific information?
    
    #### Supported testbed topology if it's a new test case?
    
    ### Documentation
    <!--
    (If it's a new feature, new test case)
    Did you update documentation/Wiki relevant to your implementation?
    Link to the wiki page?
    -->
    
    ---------
    
    Signed-off-by: Pinky Agrawal <pinky@nexthop.ai>

--- tests/saitests/py3/sai_qos_tests.py
+++ tests/saitests/py3/sai_qos_tests.py
@@ -217,6 +217,7 @@ def wait_until(timeout, interval, delay, condition, *args, **kwargs):
             condition.__name__, timeout), file=sys.stderr)
         return False
 
+
 def check_orchagent_ready(test_case):
     """Check if orchagent restart check succeeds"""
     try:
@@ -1416,8 +1417,22 @@ class DscpMappingPB(sai_base_test.ThriftInterfaceDataPlane):
                     pkt_dst_mac, src_port_mac, src_port_ip, dst_port_ip, dst_port_id,
                     src_port_id, exp_ttl, ip_ttl)
 
-            # Read Counters
-            time.sleep(3)
+            # Read Counters - wait for counters to be polled
+            def check_counters_updated():
+                """Check if queue counters have been updated"""
+                try:
+                    _, queue_results_current = sai_thrift_read_port_counters(
+                        self.dst_client, asic_type, sai_dst_port_id)
+                    # Check if any queue counter has changed from baseline
+                    return any(curr != base for curr, base in zip(queue_results_current, queue_results_base))
+                except Exception:
+                    return False
+
+            # Wait up to 10 seconds for counters to update, checking every 1 second
+            if not wait_until(10, 1, 0, check_counters_updated):
+                print("Warning: Counters may not have been updated within timeout", file=sys.stderr)
+
+            # Do a fresh read of counters
             port_results, queue_results = sai_thrift_read_port_counters(self.dst_client, asic_type, sai_dst_port_id)
 
             print(list(map(operator.sub, queue_results,
@@ -4656,7 +4671,7 @@ class PgMinThresholdTest(sai_base_test.ThriftInterfaceDataPlane):
 
             # Wait until received packets >= sent packets
             if not wait_until(10, 2, 2, check_pg_packets_received, self, self.pg0,
-                            pg_counters_base_pg0, recv_counters_base_pg0, self.pg_pkts_to_fill):
+                              pg_counters_base_pg0, recv_counters_base_pg0, self.pg_pkts_to_fill):
                 print("Warning: PG0 did not receive all packets within timeout", file=sys.stderr)
 
             # Read counters after PG0 traffic
@@ -4722,7 +4737,7 @@ class PgMinThresholdTest(sai_base_test.ThriftInterfaceDataPlane):
 
             # Wait until received packets >= sent packets
             if not wait_until(10, 2, 2, check_pg_packets_received, self, self.pg1,
-                            pg_counters_base_pg1, recv_counters_base_pg1, self.pg_pkts_to_fill):
+                              pg_counters_base_pg1, recv_counters_base_pg1, self.pg_pkts_to_fill):
                 print("Warning: PG1 did not receive all packets within timeout", file=sys.stderr)
 
             # Read counters after PG1 traffic
