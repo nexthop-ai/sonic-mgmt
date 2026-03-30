632f8a0724e471e27d571c8291e736988d72c025
vinay-nexthop <vinay@nexthop.ai>
2026-03-30 15:27:34 -0700

    NOS-5724: Use bcm provided cint script for disabling traps (#1310)
    <!--
    Please make sure you've read and understood our contributing guidelines;
    https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
    
    Please provide following information to help code review process a bit
    easier:
    -->
    ### Description of PR
    <!--
    - Please include a summary of the change and which issue is fixed.
    - Please also include relevant motivation and context. Where should
    reviewer start? background context?
    - List any dependencies that are required for this change.
    -->
    
    Summary:
    BCM provided generic cint script to disable all types of q3d traps (csp
    CS00012452955), using their version before pushing upstream.
    
    ### Type of change
    
    <!--
    - Fill x for your type of change.
    - e.g.
    - [x] Bug fix
    -->
    
    - [ ] Bug fix
    - [x] Testbed and Framework(new/improvement)
    - [ ] New Test case
        - [ ] Skipped for non-supported platforms
    - [ ] Test case improvement
    
    #### Upstream required
    
    - [ ] Yes
    - [ ] No
    
    #### Upstream requirements (if PR has "Upstream Required" label)
    
    - [ ] PR title is free of sensitive information (customer names, product
    codenames, internal references)
    
    #### Which release branch to backport/forwardport
    - [ ] 202505
    - [x] 202511
    
    ### Approach
    #### What is the motivation for this PR?
    
    #### How did you do it?
    
    #### How did you verify/test it?
    Did basic script validation on Humm dut.
    
    > cint> disable_trap(unit, bcmRxTrapIpCompMcInvalidIp,
    "IpCompMcInvalidIp");
    > disable_trap(unit, bcmRxTrapIpCompMcInvalidIp, "IpCompMcInvalidIp");
    > Disabling trap: IpCompMcInvalidIp
    > admin@humm115:~$ bcmcmd "trap list pre" | grep McIn
    > | IpComp
    
    McInvalidIp | VTT_IP_COMP_MC_INVALID_IP(0x18) | 0 | 0 | FALSE
    
    
    #### Any platform specific information?
    
    #### Supported testbed topology if it's a new test case?
    
    ### Documentation
    <!--
    (If it's a new feature, new test case)
    Did you update documentation/Wiki relevant to your implementation?
    Link to the wiki page?
    -->

--- ansible/scripts/q3d_disable_trap.c
+++ ansible/scripts/q3d_disable_trap.c
@@ -11,15 +11,30 @@ void disable_trap(int unit, bcm_rx_trap_t trap_type, char *trap_name)
 
     printf("Disabling trap: %s\n", trap_name);
 
-    rv = bcm_rx_trap_type_get(unit, 0, trap_type, &trap_id);
-    if (rv != BCM_E_NONE) {
-        printf("WARN: bcm_rx_trap_type_get failed for %s, rv=%d\n",
+    rv = bcm_rx_trap_type_create(unit, 0, trap_type, &trap_id);
+    if (rv == BCM_E_EXISTS) {
+        rv = bcm_rx_trap_type_get(unit, 0, trap_type, &trap_id);
+        if (rv != BCM_E_NONE) {
+            printf("WARN: bcm_rx_trap_type_get failed for %s, rv=%d\n",
+                   trap_name, rv);
+            return;
+        }
+    } else if (rv != BCM_E_NONE) {
+        printf("WARN: bcm_rx_trap_type_create failed for %s, rv=%d\n",
                trap_name, rv);
         return;
     }
 
+    /* Read the current config first, then only modify strengths */
     bcm_rx_trap_config_t_init(&config);
-    config.flags = 0;
+    rv = bcm_rx_trap_get(unit, trap_id, &config);
+    if (rv != BCM_E_NONE) {
+        printf("WARN: bcm_rx_trap_get failed for trap_id=0x%x (%s), rv=%d\n",
+               trap_id, trap_name, rv);
+        return;
+    }
+
+    /* Zero the strengths so this trap loses in action resolution */
     config.trap_strength = 0;
     config.snoop_strength = 0;
 
